f3b234bd324d00da1fea69e98a9a0cae1f4c25bd
강민규 <gangmingyu@gangmingyuui-MacBookAir.local>
2021-05-22 21:31:00 +0900

    로그인 UI 뼈대

--- velog-backend/src/main/java/com/kang/velogbackend/congfig/SecurityConfig.java
+++ velog-backend/src/main/java/com/kang/velogbackend/congfig/SecurityConfig.java
@@ -1,10 +1,24 @@
 package com.kang.velogbackend.congfig;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+
+
+@EnableGlobalMethodSecurity(prePostEnabled=true) // 특정 주소로 접근을 하면 권한 및 인증을 미리 체크하겠다는 뜻.
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration // ioc 등록
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder encode() {
@@ -15,13 +29,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.csrf().disable();
-        http.authorizeRequests()
-                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')") //관리자만 들어올 수 있도록
-                .anyRequest().permitAll()
+        http
+                .cors().configurationSource(corsConfigurationSource())
                 .and()
-                .formLogin()//x-www-form-urlencoded, formlogin()은 json 던지면 못 받음
-                .loginPage("/loginForm")//리다이렉션
-                .loginProcessingUrl("/login");
+                .csrf().disable()
+//                .addFilter(new JwtLoginFilter(authenticationManager()))
+//                .addFilter(new JwtVerifyFilter(authenticationManager(), userRepository))
+                .formLogin().disable()
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)// STATELESS: session을 사용하지 않겠다는 의미
+                .and()
+                .authorizeRequests()
+                .antMatchers("/user/**").access("hasRole('ROLE_USER') OR hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.addExposedHeader("Authorization");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
